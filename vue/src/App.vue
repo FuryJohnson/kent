1b6b323a782411a37237643a13fdec73519fc865
Борзунов Игорь <Borzunov@lanit>
2019-10-30 16:06:10 +0300

    backend carcass

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -37,7 +37,7 @@
   
   <b-card
     title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
+    img-src="https://pbs.twimg.com/media/EEpAUukXUAAlKBD.jpg"
     img-alt="Image"
     img-top
     tag="article"
